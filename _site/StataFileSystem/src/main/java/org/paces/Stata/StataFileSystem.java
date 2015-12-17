c5197a47752e248f21b755af7a9f8609b1cd029e
wbuchanan <william@williambuchanan.net>
2015-12-17 12:49:11 -0600

    Updated brewviewer page to show all available palettes with at least one iteration.

---
+++ _site/StataFileSystem/src/main/java/org/paces/Stata/StataFileSystem.java
@@ -0,0 +1,151 @@
+package org.paces.Stata;
+
+import com.stata.sfi.Macro;
+import com.stata.sfi.SFIToolkit;
+
+import java.io.IOException;
+
+/**
+ * @author Billy Buchanan
+ * @version 0.0.0
+ */
+public class StataFileSystem {
+
+	/***
+	 * Method used to retrieve the created, modified, and last accessed dates
+	 * for a given file
+	 * @param args First element of the string array must be the filepath,
+	 *             Second element identifies if the OS is Windoze or not,
+	 *             Third element used to print results to Stata console
+	 * @throws IOException An exception thrown due to a read/write error.
+	 */
+	public static int fileCreated(String[] args) {
+		try {
+			FileCreated x = new FileCreated(args);
+			returnProperties(x);
+			return 0;
+		} catch (IOException e) {
+			SFIToolkit.errorln(SFIToolkit.stackTraceToString(e));
+			return 1;
+		}
+	}
+
+	/***
+	 * Method to set the executable property of the file passed
+	 * @param args The file and OS information used to access the file from
+	 *                the JVM
+	 * @return A success/failure indicator
+	 */
+	public static int makeExecutable(String[] args) {
+		Boolean onoff;
+		Boolean global;
+		try {
+			FileCreated x = new FileCreated(args);
+			if (Macro.getLocalSafe("global").equals("global")) global = true;
+			else global = false;
+			if (Macro.getLocalSafe("x").equals("on")) onoff = true;
+			else onoff = false;
+			x.setExecutable(onoff, global);
+			x = new FileCreated(args);
+			returnProperties(x);
+			return 0;
+		} catch (IOException e) {
+			SFIToolkit.errorln(SFIToolkit.stackTraceToString(e));
+			return 1;
+		}
+	}
+
+	/***
+	 * Method to set the executable property of the file passed
+	 * @param args The file and OS information used to access the file from
+	 *                the JVM
+	 * @return A success/failure indicator
+	 */
+	public static int makeReadable(String[] args) {
+		Boolean onoff;
+		Boolean global;
+		try {
+			FileCreated x = new FileCreated(args);
+			if (Macro.getLocalSafe("global").equals("global")) global = true;
+			else global = false;
+			if (Macro.getLocalSafe("r").equals("on")) onoff = true;
+			else onoff = false;
+			x.setReadable(onoff, global);
+			x = new FileCreated(args);
+			returnProperties(x);
+			return 0;
+		} catch (IOException e) {
+			SFIToolkit.errorln(SFIToolkit.stackTraceToString(e));
+			return 1;
+		}
+	}
+
+	/***
+	 * Method to set the executable property of the file passed
+	 * @param args The file and OS information used to access the file from
+	 *                the JVM
+	 * @return A success/failure indicator
+	 */
+	public static int makeWritable(String[] args) {
+		Boolean global;
+		Boolean onoff;
+		try {
+			FileCreated x = new FileCreated(args);
+			if (Macro.getLocalSafe("global").equals("global")) global = true;
+			else global = false;
+			if (Macro.getLocalSafe("w").equals("on")) onoff = true;
+			else onoff = false;
+			x.setWritable(onoff, global);
+			x = new FileCreated(args);
+			returnProperties(x);
+			return 0;
+		} catch (IOException e) {
+			SFIToolkit.errorln(SFIToolkit.stackTraceToString(e));
+			return 1;
+		}
+	}
+
+
+
+	/***
+	 * Method to set the executable property of the file passed
+	 * @param args The file and OS information used to access the file from
+	 *                the JVM
+	 * @return A success/failure indicator
+	 */
+	public static int makeReadOnly(String[] args) {
+		try {
+			FileCreated x = new FileCreated(args);
+			x.setReadOnly();
+			x = new FileCreated(args);
+			returnProperties(x);
+			return 0;
+		} catch (IOException e) {
+			SFIToolkit.errorln(SFIToolkit.stackTraceToString(e));
+			return 1;
+		}
+	}
+
+	/***
+	 * Convenience method used to set return values for Stata command
+	 * @param stataFile The FileCreated object used to access and set file
+	 *                     properties.
+	 */
+	public static void returnProperties(FileCreated stataFile) {
+		Macro.setLocal("createdon", stataFile.getCreationDate());
+		Macro.setLocal("modifiedon", stataFile.getModifiedDate());
+		Macro.setLocal("accessedon", stataFile.getLastAccessedDate());
+		Macro.setLocal("symlink", stataFile.getSymLink());
+		Macro.setLocal("regfile", stataFile.getRegFile());
+		Macro.setLocal("filesize", stataFile.getFileSize());
+		Macro.setLocal("absolutepath", stataFile.getAbsolutePath());
+		Macro.setLocal("canonicalpath", stataFile.getCanonicalPath());
+		Macro.setLocal("executable", stataFile.getExecutable());
+		Macro.setLocal("filename", stataFile.getFileName());
+		Macro.setLocal("hidden", stataFile.getHidden());
+		Macro.setLocal("parent", stataFile.getParent());
+		Macro.setLocal("readable", stataFile.getReadable());
+		Macro.setLocal("writable", stataFile.getWritable());
+	} // End of Method declaration
+
+} // End of Class declaration
--- brewviewer.md
+++ brewviewer.md
@@ -36,6 +36,247 @@ brewviewer dark2 mdebar accent pastel2 set1 tableau, c(5) comb
 ![brewviewer Example 3](../img/brewviewerex3.png)
 
 
+## Gallery
+The full set of palettes (based on the maximum number of colors available in the palette), displayed with the sequential option and impaired options is available below.  
+
+### accent
+
+![brewviewer Example 4.](../img/accent_7.png)
+
+### activitiesa
+
+![brewviewer Example 5.](../img/activitiesa_5.png)
+
+### activitiest
+
+![brewviewer Example 6.](../img/activitiest_5.png)
+
+### blues
+
+![brewviewer Example 7.](../img/blues_8.png)
+
+### brandsa
+
+![brewviewer Example 8.](../img/brandsa_7.png)
+
+### brandse
+
+![brewviewer Example 9.](../img/brandse_7.png)
+
+### brbg
+
+![brewviewer Example 10.](../img/brbg_10.png)
+
+### bugn
+
+![brewviewer Example 11.](../img/bugn_8.png)
+
+### bupu
+
+![brewviewer Example 12.](../img/bupu_8.png)
+
+### carsa
+
+![brewviewer Example 13.](../img/carsa_6.png)
+
+### carse
+
+![brewviewer Example 14.](../img/carse_6.png)
+
+### category10
+
+![brewviewer Example 15.](../img/category10_10.png)
+
+### category20
+
+![brewviewer Example 16.](../img/category20_20.png)
+
+### category20b
+
+![brewviewer Example 17.](../img/category20b_20.png)
+
+### category20c
+
+![brewviewer Example 18.](../img/category20c_20.png)
+
+### dark2
+
+![brewviewer Example 19.](../img/dark2_7.png)
+
+### drinksa
+
+![brewviewer Example 20.](../img/drinksa_7.png)
+
+### drinkse
+
+![brewviewer Example 21.](../img/drinkse_7.png)
+
+### featuresa
+
+![brewviewer Example 22.](../img/featuresa_5.png)
+
+### featurest
+
+![brewviewer Example 23.](../img/featurest_5.png)
+
+### fooda
+
+![brewviewer Example 24.](../img/fooda_7.png)
+
+### foode
+
+![brewviewer Example 25.](../img/foode_7.png)
+
+### fruita
+
+![brewviewer Example 26.](../img/fruita_7.png)
+
+### fruite
+
+![brewviewer Example 27.](../img/fruite_7.png)
+
+### ggplot2
+
+![brewviewer Example 28.](../img/ggplot2_24.png)
+
+### gnbu
+
+![brewviewer Example 29.](../img/gnbu_8.png)
+
+### greens
+
+![brewviewer Example 30.](../img/greens_8.png)
+
+### greys
+
+![brewviewer Example 31.](../img/greys_8.png)
+
+### mdebar
+
+![brewviewer Example 32.](../img/mdebar_5.png)
+
+### mdepoint
+
+![brewviewer Example 33.](../img/mdepoint_3.png)
+
+### oranges
+
+![brewviewer Example 34.](../img/oranges_8.png)
+
+### orrd
+
+![brewviewer Example 35.](../img/orrd_8.png)
+
+### paired
+
+![brewviewer Example 36.](../img/paired_12.png)
+
+### pastel1
+
+![brewviewer Example 37.](../img/pastel1_8.png)
+
+### pastel2
+
+![brewviewer Example 38.](../img/pastel2_7.png)
+
+### piyg
+
+![brewviewer Example 39.](../img/piyg_10.png)
+
+### prgn
+
+![brewviewer Example 40.](../img/prgn_10.png)
+
+### pubu
+
+![brewviewer Example 41.](../img/pubu_8.png)
+
+### pubugn
+
+![brewviewer Example 42.](../img/pubugn_8.png)
+
+### puor
+
+![brewviewer Example 43.](../img/puor_10.png)
+
+### purd
+
+![brewviewer Example 44.](../img/purd_8.png)
+
+### purples
+
+![brewviewer Example 45.](../img/purples_8.png)
+
+### rdbu
+
+![brewviewer Example 46.](../img/rdbu_10.png)
+
+### rdgy
+
+![brewviewer Example 47.](../img/rdgy_10.png)
+
+### rdpu
+
+![brewviewer Example 48.](../img/rdpu_8.png)
+
+### rdylbu
+
+![brewviewer Example 49.](../img/rdylbu_10.png)
+
+### rdylgn
+
+![brewviewer Example 50.](../img/rdylgn_10.png)
+
+### reds
+
+![brewviewer Example 51.](../img/reds_8.png)
+
+### set1
+
+![brewviewer Example 52.](../img/set1_8.png)
+
+### set2
+
+![brewviewer Example 53.](../img/set2_7.png)
+
+### set3
+
+![brewviewer Example 54.](../img/set3_12.png)
+
+### spectral
+
+![brewviewer Example 55.](../img/spectral_10.png)
+
+### tableau
+
+![brewviewer Example 56.](../img/tableau_20.png)
+
+### veggiesa
+
+![brewviewer Example 57.](../img/veggiesa_7.png)
+
+### veggiese
+
+![brewviewer Example 58.](../img/veggiese_7.png)
+
+### ylgn
+
+![brewviewer Example 59.](../img/ylgn_8.png)
+
+### ylgnbu
+
+![brewviewer Example 60.](../img/ylgnbu_8.png)
+
+### ylorbr
+
+![brewviewer Example 61.](../img/ylorbr_8.png)
+
+### ylorrd
+
+![brewviewer Example 62.](../img/ylorrd_8.png)
+
+
+
 ## References
 [Bostock, M., Ogievetsky, V., & Heer, J. (2011).  D3: data driven documents. *IEEE Transactions on Visualization & Computer Graphics. 17(12)* pp 2301 - 2309. Retrieved from http://vis.stanford.edu/papers/d3](http://vis.stanford.edu/papers/d3)  
 
